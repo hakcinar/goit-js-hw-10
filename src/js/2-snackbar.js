07f52259e303520828a2dd43dd71bf3f6e1a09e0
Hakan Akcinar <hakcinar1@gmail.com>
2025-07-16 23:57:21 +0300

    Form finished

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,1 +1,34 @@
-t
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  const formData = new FormData(form);
+  const data = Object.fromEntries(formData.entries());
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (data.state === 'fulfilled') {
+        resolve('Form submitted successfully!');
+      } else {
+        reject('Form submission failed!');
+      }
+    }, data.delay);
+  });
+  promise
+    .then(message => {
+      iziToast.success({
+        title: 'Success',
+        message: message,
+        position: 'topRight',
+      });
+    })
+    .catch(error => {
+      iziToast.error({
+        title: 'Error',
+        message: error,
+        position: 'topRight',
+      });
+    });
+});
